ba3926332f6a43652f7530e0be0f44194d0e532b
tvkalex <tvkalex@yandex.ru>
2022-12-03 22:23:21 +0300

    Add Seminar2Task3 solution

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -7,30 +7,40 @@
 Console.Clear();
 System.Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine()!);
-switch (number)
-{
-    case 1:
-        System.Console.WriteLine("Нет");
-        break;
-    case 2:
-        System.Console.WriteLine("Нет");
-        break;
-    case 3:
-        System.Console.WriteLine("Нет");
-        break;
-    case 4:
-        System.Console.WriteLine("Нет");
-        break;
-    case 5:
-        System.Console.WriteLine("Нет");
-        break;
-    case 6:
-        System.Console.WriteLine("Да");
-        break;
-    case 7:
-        System.Console.WriteLine("Да");
-        break;
-    default:
-        System.Console.WriteLine("А сколько дней в твоей неделе?");
-        break;
-}
+if (number > 0 && number < 6)
+    System.Console.WriteLine("Нет");
+else if (number > 6 && number < 8)
+    System.Console.WriteLine("Да");
+else
+    System.Console.WriteLine("А сколько дней в твоей неделе?");
+
+// Console.Clear();
+// System.Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// switch (number)
+// {
+//     case 1:
+//         System.Console.WriteLine("Нет");
+//         break;
+//     case 2:
+//         System.Console.WriteLine("Нет");
+//         break;
+//     case 3:
+//         System.Console.WriteLine("Нет");
+//         break;
+//     case 4:
+//         System.Console.WriteLine("Нет");
+//         break;
+//     case 5:
+//         System.Console.WriteLine("Нет");
+//         break;
+//     case 6:
+//         System.Console.WriteLine("Да");
+//         break;
+//     case 7:
+//         System.Console.WriteLine("Да");
+//         break;
+//     default:
+//         System.Console.WriteLine("А сколько дней в твоей неделе?");
+//         break;
+// }
