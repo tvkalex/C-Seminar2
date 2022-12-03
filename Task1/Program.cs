8769760ef563dd83a17c47cd6749458c98d8fc41
tvkalex <tvkalex@yandex.ru>
2022-12-03 17:48:37 +0300

    Add Seminar2Task1 solution

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает
+// вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+System.Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+if ((num / 100 > 0) && (num / 1000 == 0))
+    Console.WriteLine(num / 10 % 10);
+else
+    Console.WriteLine("Число не трёхзначное");
+
+
+
+
