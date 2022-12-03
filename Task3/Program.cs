f7002f22d7a97f36670399412c2a0617288eb6a9
tvkalex <tvkalex@yandex.ru>
2022-12-03 21:43:18 +0300

    Add Seminar2Task3 solution

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+System.Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+switch (number)
+{
+    case 1:
+        System.Console.WriteLine("Нет");
+        break;
+    case 2:
+        System.Console.WriteLine("Нет");
+        break;
+    case 3:
+        System.Console.WriteLine("Нет");
+        break;
+    case 4:
+        System.Console.WriteLine("Нет");
+        break;
+    case 5:
+        System.Console.WriteLine("Нет");
+        break;
+    case 6:
+        System.Console.WriteLine("Да");
+        break;
+    case 7:
+        System.Console.WriteLine("Да");
+        break;
+    default:
+        System.Console.WriteLine("А сколько дней в твоей неделе?");
+        break;
+}
